ae792b8121e31b7bcaa7ea28b3af3d3e6751a966
hydropero <54414748+hydropero@users.noreply.github.com>
2020-09-03 18:54:30 -0500

    initial commit

---
+++ drugs/html_testing/testing2.rb
@@ -0,0 +1,16 @@
+let nested = {
+  a: {
+    x: "pop",
+    y: "tang",
+    z: "apple"
+  },
+  b: {
+    x: "tang",
+    ....
+
+
+person1 = { :first_name => "Raghu", :last_name => "Betina", :role => "Instructor" }
+
+
+nested_hash = { :a => {x: =>'pop' y: => 'tab' :z => 'fizz'} }
+
